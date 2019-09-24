8a2ba99ad029ca6ffcc0b2f5242a207a6ef1750b
Patrick <patrick.yrkim@gmail.com>
2019-09-23 20:42:56 -0700

    mongoDB setup

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express(); //this gives back an app object
+const mongoose = require("mongoose");
+const db = require("./config/keys.mongoURI");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+
+mongoose
+.connect(db, { useNewUrlParser: true }) // .connect returns a promise
+.then(() => console.log("Connected to mongoDB"))
+.catch(err => console.log(err));
 
 app.get("/", (requestObj, responseObj) => { //app listening for get requests
+    // debugger
     responseObj.send("Hello a/A!");
 });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 // tell the object to listen to a given port
 // const port = 5000;
---
+++ routes/api/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router(); //gets a router object off the router
+
+router.get("/test", (req, res) => {
+    res.json({ ms: "this is the user route" }); //sends back a json string that looks like the string passed in
+});
+
+module.exports = router;
